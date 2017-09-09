a4daad6e612e1b381dac3874c7dcb2ac8b7e5f91
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-09 21:13:35 +0300

    Add health component script Add test scene Add utility script for dealing damage when clicking object

---
+++ Assets/Scripts/Components/HealthComponent.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+    private bool _isAlive;
+    private bool _isDamaged;
+
+    private void Start()
+    {
+        _isAlive = true;
+        CurrentHealth = MaxHealth;
+    }
+
+    private void Update()
+    {
+        if (_isDamaged)
+        {
+            Debug.Log("Damage handled");
+
+            _isDamaged = false;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (_isAlive)
+        {
+            Debug.Log("Damage taken");
+
+            _isDamaged = true;
+            CurrentHealth -= amount;
+
+            if (CurrentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Object died");
+
+        _isAlive = false;
+    }
+}
