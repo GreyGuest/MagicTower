d3e065c6801104a948c140c290bea5d6eeab7611
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-16 19:06:53 +0300

    Add cooldown class

---
+++ Assets/Scripts/Spells/Cooldown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Cooldown
+{
+    private readonly float cooldownSeconds;
+    private float lastCastTime;
+
+    public Cooldown(float seconds)
+    {
+        cooldownSeconds = seconds;
+        lastCastTime = -cooldownSeconds;
+    }
+
+    public bool IsInProgress()
+    {
+        return Time.time < lastCastTime + cooldownSeconds;
+    }
+
+    public void Start()
+    {
+        lastCastTime = Time.time;
+    }
+    
+    public float GetRemainingValue()
+    {
+        var remainingCooldown = lastCastTime + cooldownSeconds - Time.time;
+        return remainingCooldown > 0 ? remainingCooldown : 0;
+    }
+}
