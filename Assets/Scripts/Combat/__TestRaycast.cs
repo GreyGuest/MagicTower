57a3a70723c24626d10a3a4c2fedadfde76db495
GreyGuest <undredem@gmail.com>
2017-09-22 20:47:45 +0300

    add VFX & update graphical settings

---
+++ Assets/Scripts/Combat/__TestRaycast.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class __TestRaycast : MonoBehaviour {
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("We hit" + hit.collider.name + " " + hit.point);
+            }
+        }
+	}
+}
