35de5ec9ca1588f54ce7c5aa8d55e87902e33f1b
GreyGuest <undredem@gmail.com>
2017-10-04 14:01:07 +0300

    added new fireball

---
+++ Assets/Scripts/GraphicProfile/windmill_PaddleRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windmill_PaddleRotation : MonoBehaviour {
+    public Vector3 Rotation;
+    public float speed;
+    // Use this for initialization
+    Quaternion startRotation;
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		transform.Rotate(Rotation * Time.deltaTime * speed);
+	}
+}
